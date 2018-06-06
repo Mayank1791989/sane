cb9923024538e17c1164c5e908bc4a4821ef0762
Stefan Penner <stefan.penner@gmail.com>
2018-06-06 11:01:01 -0700

    add watch-detector

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var NodeWatcher = require('./src/node_watcher');
 var PollWatcher = require('./src/poll_watcher');
 var WatchmanWatcher = require('./src/watchman_watcher');
 var FSEventsWatcher = require('./src/fsevents_watcher');
+var WatchDetector = require('watch-detector');
 
 function sane(dir, options) {
   options = options || {};
@@ -16,6 +17,18 @@ function sane(dir, options) {
     return new WatchmanWatcher(dir, options);
   } else if (options.fsevents) {
     return new FSEventsWatcher(dir, options);
+  } else if (options.auto) {
+    delete options.auto;
+    var detector = new WatchDetector({
+      fs: require('fs'),
+      root: dir,
+      ui: {
+        writeLine(message) {
+          console.log(message);
+        },
+      },
+    });
+    return sane(dir, detector.findBestWatcherOption(options));
   } else {
     return new NodeWatcher(dir, options);
   }
